78269f93a2da5be7435d83534173bb3b72782507
lilgodman210 <rybug5210@gmail.com>
2024-09-25 18:15:54 -0400

    Fire Add

---
+++ README.md
@@ -0,0 +1,20 @@
+* [Live Demo](https://imswarnil.github.io/Youtuber-Portfolio-Material-Website/) -Youtuber Portfolio Material Website
+
+# One Page Youtuber Portfolio Template Using MateriallizeCSS
+Website Template For Content Creator Specially Youtubers, Youtubers need website as being a youtuber i understand what they want in their website or portfolio i have designed this template in such a way so that creator can create his own beautiful portfolio for exiting template you can do further modification you free to use this template for any other purpose if you want.
+
+One Page Youtuber Portfolio Template
+## Built With
+
+* [MateriallizeCSS](http://www.materiallizecss.com) - Material Design By Google
+
+## Read My Blog
+If you love my project you can visit my blog for more awesome work 
+* [Imswarnil :- Blog Of Trailblazer ](https://imswarnil.com) - My Dev Blog
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+
+
---
+++ vendor/custom.js
@@ -0,0 +1,49 @@
+M.AutoInit();
+var instance = M.Carousel.init({
+    fullWidth: true
+  });
+  
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+  // Or with jQuery
+
+
+  
+  var scroll = new SmoothScroll('a[href*="#"]');
+   
+/* 
+This Code Conflict between animate smoth and tabs of material
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});   
+*/
